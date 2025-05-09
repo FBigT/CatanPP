5675f04424e513968a609889d2dae0f77974a2e4
babiclara <lara3211@gmail.com>
2025-05-09 12:59:47 +0200

    TradePutInGameModeCampaign

---
+++ Assets/Scripts/GameMode/Trading/TradeBackButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GameMode.Trading
+{
+    [RequireComponent(typeof(Button))]
+    public class TradeBackButton : MonoBehaviour
+    {
+        [Tooltip("Drag the MainTradePanel GameObject here")]
+        [SerializeField] private GameObject mainTradePanel;
+
+        void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                if (mainTradePanel != null)
+                    mainTradePanel.SetActive(false);
+                else
+                    Debug.LogWarning($"[TradeBackButtonUI] mainTradePanel not assigned on {gameObject.name}");
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/GameMode/Trading/TradeLauncherButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GameMode.Trading
+{
+    [RequireComponent(typeof(Button))]
+    public class TradeLauncherButton : MonoBehaviour
+    {
+        [Header("Panels")]
+        [Tooltip("Drag in your Main Trade Panel (the one you copied over)")]
+        [SerializeField] private GameObject mainTradePanel;
+
+        void Awake()
+        {
+            if (mainTradePanel != null)
+                mainTradePanel.SetActive(false);
+
+            GetComponent<Button>().onClick.AddListener(OpenTradePanel);
+        }
+
+        private void OpenTradePanel()
+        {
+            if (mainTradePanel != null)
+            {
+                mainTradePanel.SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning($"[TradeLauncherUGUI] mainTradePanel not assigned on {gameObject.name}");
+            }
+        }
+    }
+}
